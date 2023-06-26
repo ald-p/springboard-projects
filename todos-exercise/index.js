ab0063737b40ae2f227eaf84892dbe7fd4e705b6
Aldwin <aldwin.pag@gmail.com>
2023-06-26 00:33:27 -0400

    Add localStorage functionality to to-do app

--- todos-exercise/index.js
+++ todos-exercise/index.js
@@ -1,10 +1,12 @@
 const formEl = document.querySelector('#form');
 const textInput = document.querySelector('#todo-input');
 const toDoList = document.querySelector('#todo-list');
+let toDoArr = [];
 
-const createToDoItemElement = (toDo) => {
+const createToDoItemElement = (toDo, id) => {
   const div = document.createElement('div');
   div.classList.add('todo-item');
+  div.id = id;
 
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
@@ -24,19 +26,48 @@ const createToDoItemElement = (toDo) => {
   return div;
 }
 
+if (localStorage.getItem('toDoList')) {
+  toDoArr = JSON.parse(localStorage.getItem('toDoList'));
+  for (const toDo of toDoArr) {
+    const toDoEl = createToDoItemElement(toDo.text, toDo.id);
+    toDoList.appendChild(toDoEl);
+    if (toDo.isCompleted) {
+      toDoEl.classList.add('completed');
+      toDoEl.firstElementChild.checked = true;
+    }
+  }
+};
+
 // add to-do
 formEl.addEventListener('submit', function(e) {
   e.preventDefault();
   const inputValue = textInput.value;
-  const newToDoEl = createToDoItemElement(inputValue);
+  const newToDoEl = createToDoItemElement(inputValue, Date.now());
   toDoList.appendChild(newToDoEl);
+
+  toDoArr.push({
+    id: newToDoEl.id, 
+    isCompleted: false, 
+    text: inputValue});
+  localStorage.setItem('toDoList', JSON.stringify(toDoArr));
+
   textInput.value = '';
 });
 
 // complete to-do
 toDoList.addEventListener('change', function(e) {
-  const toDoItemEl = e.target.parentElement;
-  toDoItemEl.classList.toggle('completed');
+  const currentToDoItem = e.target.parentElement;
+  currentToDoItem.classList.toggle('completed');
+
+  const idx = toDoArr.findIndex( toDo => currentToDoItem.id === toDo.id);
+
+  if (toDoArr[idx].isCompleted) {
+    toDoArr[idx].isCompleted = false;
+  } else {
+    toDoArr[idx].isCompleted = true;
+  }
+
+  localStorage.setItem('toDoList', JSON.stringify(toDoArr));
 });
 
 // delete to-do
@@ -44,5 +75,9 @@ toDoList.addEventListener('click', function(e) {
   if (e.target.className === 'delete-btn') {
     const toDoItemEl = e.target.parentElement;
     toDoItemEl.remove();
+
+    toDoArr = toDoArr.filter( toDo => toDoItemEl.id !== toDo.id);
+
+    localStorage.setItem('toDoList', JSON.stringify(toDoArr));
   }
 });
